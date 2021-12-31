3d5d591ac3b500a346a7816ac3cb50bf3c6559d2
Dylan Faelker <84638978+dylpykill@users.noreply.github.com>
2021-12-30 23:35:32 -0500

    Add files via upload

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,7 @@
 const Square = ({ square, onSelect, moves, selectedNum, }) => {
     return (
         <div 
-            className={selectedNum === square.id ? 'square-chosen' : (moves.includes(square.id) ? 'square-highlight' : (((square.id%8 + Math.floor((square.id-1) / 8))%2)  ? 'square-black' : "square-white"))}
+            className={selectedNum === square.id ? 'square-chosen' : (moves.includes(square.id) ? 'square-highlight' : (((square.id%8 + Math.floor((square.id-1) / 8))%2)  ? 'square-white' : "square-black"))}
             onClick={() => onSelect(square)}
         >
             {square.icon}
